0b90f599d7abe9e1250b1fa81612d91354072f32
dhabensky <dhabensky@yandex.ru>
2018-12-15 23:53:35 +0300

    add lost class AnimationFrame

---
+++ app/src/main/java/com/aahack/yojump/util/AnimationFrame.kt
@@ -0,0 +1,12 @@
+package com.aahack.yojump.util
+
+import android.graphics.drawable.Drawable
+
+/**
+ * Created on 15.12.2018.
+ * @author dhabensky <dhabensky@yandex.ru>
+ */
+class AnimationFrame(
+		val drawable: Drawable,
+		val duration: Long
+)
