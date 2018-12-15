e9c643d6e482f8f65cee92c38522adac46473021
Ruslan Khomyakov <azley@mail.ru>
2018-12-15 20:48:25 +0300

    player added velocity and gravity

--- app/src/main/java/com/aahack/yojump/Player.java
+++ app/src/main/java/com/aahack/yojump/Player.java
@@ -5,10 +5,12 @@ import android.graphics.drawable.Drawable;
 
 import org.jetbrains.annotations.NotNull;
 
-public class Player extends GameObject {
+public class Player extends GameObject implements JumpListener {
 
 	public Drawable player;
 	public int left, right, top, bottom;
+	public int velocity = 0, gravity =5;
+	int playerX, playerY;
 
 	@Override
 	public void render(@NotNull Canvas canvas) {
@@ -16,9 +18,14 @@ public class Player extends GameObject {
 
 		player.setBounds(left, top, right, bottom);
 		player.draw(canvas);
+
+		velocity += gravity;
+		playerY += velocity;
 	}
 
 
-
-
+	@Override
+	public void onJump() {
+		velocity = -40;
+	}
 }
