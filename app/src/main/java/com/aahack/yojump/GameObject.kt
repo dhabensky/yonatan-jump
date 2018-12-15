07867c6847d242ed89d412b62593abb08cf97e6e
dhabensky <dhabensky@yandex.ru>
2018-12-15 20:04:43 +0300

    Add gameobject class

---
+++ app/src/main/java/com/aahack/yojump/GameObject.kt
@@ -0,0 +1,18 @@
+package com.aahack.yojump
+
+import android.graphics.Canvas
+import android.graphics.PointF
+
+/**
+ * Created on 15.12.2018.
+ * @author dhabensky <dhabensky@yandex.ru>
+ */
+class GameObject {
+
+	var pos = PointF()
+
+	fun render(canvas: Canvas) {
+
+	}
+
+}
