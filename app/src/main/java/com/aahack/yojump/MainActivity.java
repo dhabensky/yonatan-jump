434314062a6f2bce30b9674b5b9c90bb4d9b974a
dhabensky <dhabensky@yandex.ru>
2018-12-16 10:32:05 +0300

    faders

--- app/src/main/java/com/aahack/yojump/MainActivity.java
+++ app/src/main/java/com/aahack/yojump/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+	    requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+			    WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         startButton = findViewById(R.id.start);
         startButton.setOnClickListener(new View.OnClickListener() {
