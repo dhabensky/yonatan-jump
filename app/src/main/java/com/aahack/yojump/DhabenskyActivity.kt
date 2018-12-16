b709348ac297d9ab091fb4d031d60a9ecffc1349
dhabensky <dhabensky@yandex.ru>
2018-12-16 05:03:34 +0300

    restore balance

--- app/src/main/java/com/aahack/yojump/DhabenskyActivity.kt
+++ app/src/main/java/com/aahack/yojump/DhabenskyActivity.kt
@@ -81,11 +81,11 @@ class DhabenskyActivity : AppCompatActivity() {
 		val player = Player()
 		player.jumpVelocity = 1000
 		player.frames = createPlayerFrames()
-		player.pos.set(100f, 500f)
-		player.acceleration.y = 500f
-		player.w = 100
-		player.h = 100
-		player.velocity.set(400f, -30f)
+		player.pos.set(100f, 400f)
+		player.acceleration.y = 2000f
+		player.w = 200
+		player.h = 200
+		player.velocity.set(600f, 0f)
 		player.jumpSound = MediaPlayer.create(this, R.raw.jump)
 		player.jumpSound.setVolume(1f, 1f)
 		return player
