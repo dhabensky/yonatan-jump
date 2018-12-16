4e44d14aba898b41b9bee785d8467d8f3aec9e50
dhabensky <dhabensky@yandex.ru>
2018-12-16 13:44:38 +0300

    Fix permissions

--- app/src/main/java/com/aahack/yojump/MainActivity.java
+++ app/src/main/java/com/aahack/yojump/MainActivity.java
@@ -13,91 +13,67 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1001;
-    private Button startButton;
+	private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1001;
+	private Button startButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-	    requestWindowFeature(Window.FEATURE_NO_TITLE);
-	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-			    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-        startButton = findViewById(R.id.start);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_main);
+		startButton = findViewById(R.id.start);
+		startButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
 
-                checkermission();
+				checkPermission();
 
                 /*Intent intent = new Intent(MainActivity.this, DhabenskyActivity.class);
                 startActivity(intent);*/
-            }
-        });
-    }
+			}
+		});
+	}
 
-    private void checkermission() {
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
-				Manifest.permission.CAMERA)
-				!= PackageManager.PERMISSION_GRANTED) {
+	private void checkPermission() {
+		// Here, thisActivity is the current activity
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+					MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
 
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION) || ActivityCompat.shouldShowRequestPermissionRationale(this,
-					Manifest.permission.CAMERA)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.CAMERA},
-                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
+		}
+		else {
 			Intent intent = new Intent(MainActivity.this, DhabenskyActivity.class);
 			startActivity(intent);
-        }
+		}
+	}
 
-		Intent intent = new Intent(MainActivity.this, DhabenskyActivity.class);
-		startActivity(intent);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
+	@Override
+	public void onRequestPermissionsResult(int requestCode,
+	                                       String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+				// If request is cancelled, the result arrays are empty.
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					// permission was granted, yay! Do the
+					// contacts-related task you need to do.
 					Intent intent = new Intent(MainActivity.this, DhabenskyActivity.class);
 					startActivity(intent);
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+				} else {
+					// permission denied, boo! Disable the
+					// functionality that depends on this permission.
 					Intent intent = new Intent(MainActivity.this, DhabenskyActivity.class);
 					startActivity(intent);
-                }
-                return;
-            }
+				}
+				return;
+			}
 
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
-    }
+			// other 'case' lines to check for other
+			// permissions this app might request.
+		}
+	}
 
 
 }
